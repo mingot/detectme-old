cf68a95d00732ab4171c416b1f7195eef15bdf36
Josep Marc Mingot Hidalgo <jm.mingot@gmail.com>
2013-02-08 08:48:56 -0500

    Initial Commit

---
+++ DetectMe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DetectMe
+//
+//  Created by Josep Marc Mingot Hidalgo on 08/02/13.
+//  Copyright (c) 2013 Josep Marc Mingot Hidalgo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DetectMe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DetectMe
+//
+//  Created by Josep Marc Mingot Hidalgo on 08/02/13.
+//  Copyright (c) 2013 Josep Marc Mingot Hidalgo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
